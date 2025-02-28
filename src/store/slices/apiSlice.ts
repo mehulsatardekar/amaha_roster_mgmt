4db88560fb530d7db9c84890e7d304f5e5aa2408
mehulsatardekar <satardekarmehul042197@gmail.com>
2025-02-28 20:42:09 +0530

    fix:adds env variable for api routes

--- src/store/slices/apiSlice.ts
+++ src/store/slices/apiSlice.ts
@@ -2,14 +2,20 @@ import { FetchUsersResponse } from "@/src/types/UsersDetails";
 import { FetchUsersSlotsAndEventsResponse } from "@/src/types/UsersSlotsAndEvents";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://192.168.0.114:3000/api";
+
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://192.168.0.114:3000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     fetchUsers: builder.query<FetchUsersResponse, void>({
       query: () => "fetchUsers",
     }),
-    fetchUsersSlotsAndEvents: builder.query<FetchUsersSlotsAndEventsResponse, void>({
+    fetchUsersSlotsAndEvents: builder.query<
+      FetchUsersSlotsAndEventsResponse,
+      void
+    >({
       query: () => "fetchUsersSlotsAndEvents",
     }),
   }),
